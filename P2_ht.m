7753fac6a123fad5d6f4aa7b84da5be6e1925541
Eric <edm54@case.edu>
2019-10-13 23:32:03 -0400

    added forced pipe convection

--- P2_ht.m
+++ P2_ht.m
@@ -3,7 +3,7 @@ clc
 close all
 %[q, t] = radiation(306, 295, 420, 1.5)
 
-global alp kin_v  Pr k g B
+global alp kin_v  Pr k g B Dynamic_V rho
 g = 9.81; %m/s 
 
 Ts = 160 + 273;
@@ -28,6 +28,9 @@ alp = 38.3e-6;
 
 %L   Thermal conductivity [W/(m K)]
 k = refpropm('L','T',Ti,'P',101, 'Air.ppf')
+
+%D   Density
+rho = refpropm('D','T',Ti,'P',101, 'Air.ppf')
 
 faces = 4 ;
 t_total = 7 * 60; % seconds
@@ -74,7 +77,14 @@ t_forced_t= 45; % naturally convect same
 
 %% Radiation
 t_rad = 7 * 60;
-[q_rad, Q_rad] = radiation(Ts, Ti, t_rad, 2.*area_vert);
+% strongly oxidized could .95
+[q_rad, Q_rad] = radiation(Ts, Ti, t_rad, 2.*area_vert, .4);
+
+%% Forced through cylinder
+t_f = 5 %sec
+
+[q_forced_pipe, Q_forced_pipe] = forced_pipe_convection(Ts, Ti,t_f)
+ 
 
 %% New Calcs
 % 
@@ -82,8 +92,8 @@ t_rad_n = 6 * 60
 t_vert_n = 6 * 60
 t_bottom = 2 * 60
 t_top = 1.5 * 60
-
-[q_rad_n, Q_rad_n] = radiation(Ts, Ti, t_rad_n , 2.*area_vert);
+%newly polished could be .2
+[q_rad_n, Q_rad_n] = radiation(Ts, Ti, t_rad_n , 2.*area_vert, .2);
 [q_vertical_n, Q_vertical_n] = vertical_wall_convection(Ts, Ti, t_vert_n,  l_vert, area_vert)
 [q_top_n, Q_top_n] = free_convection_down(Ts, Ti, t_top, l_flat, area_flat);
 [q_bottom_n, Q_bottom_n] = free_convection_up(Ts, Ti, t_bottom, l_flat, area_flat)
@@ -91,7 +101,7 @@ t_top = 1.5 * 60
 
 %%
 Q_absolute_total = Q_forced + Q_bottom + Q_float ...
-                 + Q_top + 2*Q_vertical + Q_rad
+                 + Q_top + 2*Q_vertical + Q_rad + Q_forced_pipe
 
 Q_new_total = Q_forced  + Q_bottom_n + Q_float ...
                  + Q_top_n + 2*Q_vertical_n + Q_rad_n
@@ -100,46 +110,50 @@ savings = (Q_absolute_total - Q_new_total)/Q_absolute_total
      
 Q_loss = Q_absolute_total - Q_new_total
 
+
+
+
 figure
+%labels = {'Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation', 'Forced Pipe'}
 pie([Q_forced Q_bottom Q_float ...
-                 Q_top  2*Q_vertical  Q_rad])
-legend('Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation')
+                 Q_top  2*Q_vertical  Q_rad, Q_forced_pipe])
+legend('Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation', 'Forced Pipe')
+
 title('Original Heat Transfer')
 
 figure
 pie([Q_forced Q_bottom_n Q_float ...
-                 Q_top_n  2*Q_vertical_n  Q_rad_n Q_loss])
+                 Q_top_n  2*Q_vertical_n  Q_rad_n Q_loss], [0 0 0 0 0 0 1])
 legend('Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation', 'Savings')
 title('New Heat Transfer')
 
 
 figure
 pie([q_forced q_bottom q_float ...
-                 q_top  2*q_vertical  q_rad])
-legend('Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation')
+                 q_top  2*q_vertical  q_rad, q_forced_pipe])
+legend('Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation', 'Forced Pipe')
 title('q')
 
 %%
-total_toys_per_machine = 24 * 60 /7
-time_with_new_cycle = total_toys_per_machine * 6/60
-time_saved = 24 - time_with_new_cycle
+total_toys_per_machine = 24 * 60 /7;
+time_with_new_cycle = total_toys_per_machine * 6/60;
+time_saved = 24 - time_with_new_cycle;
 % Min number of machines to turn off one
-min_machines = 24/time_saved + 1
+min_machines = 24/time_saved + 1;
 
 % Heatloss per cycle * cylces per machine * machines
-heat_loss_daily_original = Q_absolute_total * total_toys_per_machine * 8
-heat_loss_daily_new = Q_new_total * total_toys_per_machine * 7
+heat_loss_daily_original = Q_absolute_total * total_toys_per_machine * 8;
+heat_loss_daily_new = Q_new_total * total_toys_per_machine * 7;
 
 total_savings = (heat_loss_daily_original - heat_loss_daily_new)/heat_loss_daily_original
-
-
+%%
 function[q_vert, Q_total ] = vertical_wall_convection(Ts,Ti,time, l_vert, area_vert)
 global alp kin_v Pr k g B
 % Vertical walls
 Ra_vert = (g * B * (Ts - Ti).*l_vert.^3)/(alp * kin_v);
 nusselt_vert = (.825 + (.387.*Ra_vert.^(1/6))/(1 + (.492/Pr)^(9/16))^(8/27)).^2;
 
-h_vert = nusselt_vert * k./l_vert
+h_vert = nusselt_vert * k./l_vert;
 q_vert = h_vert .* area_vert * (Ts - Ti); % watts 
 % copy code, for diff length
 Q_total = q_vert * time ;
@@ -147,7 +161,8 @@ Q_total = q_vert * time ;
 end
 
 function [q_forced, Q_total] = forced_convection(Ts, Ti, time, L_forced, area_forced)
-velo2=200; %m/s
+velo2=100; %m/s
+%velo2=200; %m/s
 global kin_v Pr k 
 %L_forced = .05 %char len
 %area_forced = .025
@@ -155,8 +170,8 @@ global kin_v Pr k
 % Recalculate kin_V, Pr
 Rex = velo2 * L_forced/kin_v;
 
-nusselt_forced = .0296 * Rex ^.8 * Pr ^(1/3);
-h_forced = nusselt_forced * k./L_forced;
+nusselt_forced = .0296 * Rex ^.8 * Pr ^(1/3)
+h_forced = nusselt_forced * k./L_forced
 q_forced = h_forced .* area_forced * (Ts - Ti);
 Q_total = q_forced * time;
 end
@@ -167,17 +182,17 @@ function [q_down, Q_total] = free_convection_down(Ts, Ti, time, L, area_flat)
 %same L and A as bottom  plate
 global alp kin_v k g B
 
-Ra_down = (g * B * (Ts - Ti).*L.^3)/(alp * kin_v)
-nusselt_down = .52 * Ra_down^(1/5)
-h_down = nusselt_down * k./L
+Ra_down = (g * B * (Ts - Ti).*L.^3)/(alp * kin_v);
+nusselt_down = .52 * Ra_down^(1/5);
+h_down = nusselt_down * k./L;
 q_down = h_down .* area_flat * (Ts - Ti); % watts 
 Q_total = q_down * time;
 end
 
 
-function [q_rad, Q_total] = radiation(Ts, Ti, time, area_vert)
-emiss= .3;
-boltzman= 5.67e-8; %W/(m^2K^4)
+function [q_rad, Q_total] = radiation(Ts, Ti, time, area_vert, emiss)
+
+boltzman= 5.67e-8; %W/(m^2K^4);
 q_rad= emiss*boltzman*(Ts^4-Ti^4)*area_vert;
 Q_total  = q_rad * time;
 end
@@ -187,9 +202,25 @@ function [q_free_up, Q_total] = free_convection_up(Ts, Ti, time, l_flat, area_fl
 global g B alp kin_v k
 Ra_flat = (g * B * (Ts - Ti).*l_flat.^3)/(alp * kin_v);
 nusselt_flat = .15 * Ra_flat.^(1/3);
-h_up = nusselt_flat * k./l_flat
+h_up = nusselt_flat * k./l_flat;
 q_free_up = h_up .* area_flat * (Ts - Ti); % watts 
 Q_total = q_free_up * time;
+
+end
+
+function [q_pipe_convection, Q_pipe_convection] = forced_pipe_convection(Ts, Ti, time)
+    global rho Pr k
+    Dynamic_V = refpropm('V','T',Ti,'P',101, 'Air.ppf');
+    V = 50 %m/s, average
+    D = 5/1000; %m --> 5mm
+    length = 30/1000; %--> 30 mm
+    Re = rho * V * D/Dynamic_V;
+    
+    Nu = .023 * Re^.8 * Pr^.4
+    h = Nu * k/ D % w/m^2 K
+    area_affected = pi * D * length
+    q_pipe_convection = 30 *h .* area_affected * (Ts - Ti); % watts 
+    Q_pipe_convection = q_pipe_convection * time;
 
 end
 
