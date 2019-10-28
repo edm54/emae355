2a41712c8c3aed3978997ed0a7f8ec1c357659ca
Eric <edm54@case.edu>
2019-10-28 00:30:34 -0400

    clean up clean up everybody everywhere

--- P2_ht.m
+++ P2_ht.m
@@ -21,23 +21,20 @@ Dynamic_V = refpropm('V','T',Ti,'P',101, 'Air.ppf');
 % Kinematic viscosity [cm^2/s], Nu
 %Kin_v = Dynamic_v/rho
 kin_v = refpropm('$','T',Ti,'P',101, 'Air.ppf');
-kin_v = kin_v /100^2 % Convert to square meters
+kin_v = kin_v /100^2; % Convert to square meters
 
 % Thermal diffusivity [cm^2/s], alpha
 alp = refpropm('%','T',Ti,'P',101, 'Air.ppf');
 alp = alp/ 100^2; % Convert to square meters
 
 % Thermal conductivity [W/(m K)]
-k = refpropm('L','T',Ti,'P',101, 'Air.ppf')
+k = refpropm('L','T',Ti,'P',101, 'Air.ppf');
 
 % Density
-rho = refpropm('D','T',Ti,'P',101, 'Air.ppf')
+rho = refpropm('D','T',Ti,'P',101, 'Air.ppf');
 
 faces = 4 ;
 t_total = 7 * 60; % second
-
-%k = .525 % w/m from us department of commerce, cast iron 
-%k = 80 % w/mk from a table carter found, young ch 15 
 
 %% Inout Paramaters
 
@@ -50,25 +47,25 @@ l_flat = area_flat / perimeter_flat;
 area_vert = .65/2; %meter^2;
 perimeter_vert = 8.32/2;
 % Characteristic length from drawings given
-l_vert = .085 %m
+l_vert = .085; %m
 
 %% Free Convection From Vertical Walls
 t_vert = 7*60; %sec
-[q_vertical, Q_vertical] = vertical_wall_convection(Ts, Ti, t_vert, l_vert,  area_vert)
+[q_vertical, Q_vertical] = vertical_wall_convection(Ts, Ti, t_vert, l_vert,  area_vert);
 
 %% Convection from Upper Surface of Hot Plate
 t_bottom_convection= 3*60;
-[q_bottom, Q_bottom] = free_convection_up(Ts, Ti, t_bottom_convection, l_flat, area_flat)
+[q_bottom, Q_bottom] = free_convection_up(Ts, Ti, t_bottom_convection, l_flat, area_flat);
 
 %% Convection while top plate is floating (i.e top and bottom surfaces fully exposed)
 t_float_t= 30; % seconds
 
 % Convection losses from top and bottom of the hot plate
-[q_float_b, Q_float_b] =  free_convection_down(Ts, Ti, t_float_t, l_flat, area_flat)
-[q_float_t, Q_float_t] = free_convection_up(Ts, Ti, t_float_t, l_flat, area_flat)
+[q_float_b, Q_float_b] =  free_convection_down(Ts, Ti, t_float_t, l_flat, area_flat);
+[q_float_t, Q_float_t] = free_convection_up(Ts, Ti, t_float_t, l_flat, area_flat);
 
 q_float = q_float_b  + q_float_t;
-Q_float = Q_float_b + Q_float_t
+Q_float = Q_float_b + Q_float_t;
 
 %% Convection from the bottom surface of a hot plate
 t_open_t= 2.5*60; % seconds
@@ -76,48 +73,46 @@ t_open_t= 2.5*60; % seconds
 
 %% Forced convection due to spraying surface with compressed air
 t_forced_t= 45; % naturally convect same 
-[q_forced, Q_forced] = forced_convection(Ts, Ti, t_forced_t, .1*l_flat, .1*area_flat)
+[q_forced, Q_forced] = forced_convection(Ts, Ti, t_forced_t, .1*l_flat, .1*area_flat);
 
 %% Radiation losses
 t_rad = 7 * 60;
-% emissivity estimated to be .4 from Dr. Hostler
 [q_rad, Q_rad] = radiation(Ts, Ti, t_rad, 2.*area_vert, .45);
 
 %% Forced Convection from air flowing through platen
-t_f = 5 %sec
-[q_forced_pipe, Q_forced_pipe] = forced_pipe_convection(Ts, Ti,t_f)
+t_f = 5; %sec
+[q_forced_pipe, Q_forced_pipe] = forced_pipe_convection(Ts, Ti,t_f);
 
 %% New Calcs
-t_rad_n = 6 * 60
-t_vert_n = 6 * 60
-t_bottom = 2 * 60
-t_top = 1.5 * 60
+t_rad_n = 6 * 60;
+t_vert_n = 6 * 60;
+t_bottom = 2 * 60;
+t_top = 1.5 * 60;
 
 % Solec LO/MIT 1 has emissivity .145
 [q_rad_n, Q_rad_n] = radiation(Ts, Ti, t_rad_n , 2.*area_vert, .145);
-[q_vertical_n, Q_vertical_n] = vertical_wall_convection(Ts, Ti, t_vert_n,  l_vert, area_vert)
+[q_vertical_n, Q_vertical_n] = vertical_wall_convection(Ts, Ti, t_vert_n,  l_vert, area_vert);
 [q_top_n, Q_top_n] = free_convection_down(Ts, Ti, t_top, l_flat, area_flat);
-[q_bottom_n, Q_bottom_n] = free_convection_up(Ts, Ti, t_bottom, l_flat, area_flat)
+[q_bottom_n, Q_bottom_n] = free_convection_up(Ts, Ti, t_bottom, l_flat, area_flat);
 
 %% Savings calculations
 Q_old_total = Q_forced + Q_bottom + Q_float ...
-                 + Q_top + 2*Q_vertical + Q_rad + Q_forced_pipe          
-Q_old_top = Q_float_t + Q_top + .5 * Q_rad + Q_forced + Q_vertical + Q_float_b
-Q_old_bot = Q_bottom + Q_vertical + .5 * Q_rad + Q_forced_pipe
+                 + Q_top + 2*Q_vertical + Q_rad + Q_forced_pipe;          
+Q_old_top = Q_float_t + Q_top + .5 * Q_rad + Q_forced + Q_vertical + Q_float_b;
+Q_old_bot = Q_bottom + Q_vertical + .5 * Q_rad + Q_forced_pipe;
      
 Q_new_total = Q_forced  + Q_bottom_n + Q_float ...
-                 + Q_top_n + 2*Q_vertical_n + Q_rad_n             
-Q_new_top = Q_float_t + Q_top_n + .5 * Q_rad_n + Q_forced + Q_vertical_n +Q_float_b 
-Q_new_bot = Q_bottom_n + Q_vertical_n + .5 * Q_rad_n           
+                 + Q_top_n + 2*Q_vertical_n + Q_rad_n;       
+Q_new_top = Q_float_t + Q_top_n + .5 * Q_rad_n + Q_forced + Q_vertical_n +Q_float_b;
+Q_new_bot = Q_bottom_n + Q_vertical_n + .5 * Q_rad_n;          
 
 savings = (Q_old_total - Q_new_total)/Q_old_total     
-Q_loss = Q_old_total - Q_new_total
+Q_loss = Q_old_total - Q_new_total;
 cycles = 24 * 60 /7;
-%heat_loss_daily_original = Q_old_total * cycles * 7;
-%heat_loss_daily_new = Q_new_total * cycles * 6;
+
 heat_loss_daily_original = Q_old_total * cycles ;
 heat_loss_daily_new = Q_new_total * cycles ;
-heat_loss_daily_savings = heat_loss_daily_original - heat_loss_daily_new
+heat_loss_daily_savings = heat_loss_daily_original - heat_loss_daily_new;
 
 
 %%
@@ -130,11 +125,9 @@ time_saved = 24 - time_with_new_cycle;
 % Min number of machines to turn off one
 min_machines = 24/time_saved;
 
-daily_amount_saved_pm = (heat_loss_daily_original - heat_loss_daily_new)/7
-cycles = total_toys_per_machine/30
-total_savings1 = ((7*heat_loss_daily_original - 6*heat_loss_daily_new)/6)
-cycle_amt_saved = total_savings1/cycles
-total_savings = ((7*heat_loss_daily_original - 6*heat_loss_daily_new)/(7*heat_loss_daily_original))
+daily_amount_saved_pm = (heat_loss_daily_original - heat_loss_daily_new)/7;
+cycles = total_toys_per_machine/30;
+
 %% Payback:
 % an electricity rate of 6¢/kWhr for use in the payback period estimate
 machines = 1; % Does not effect final answer
@@ -143,7 +136,7 @@ surface_area_needed = 1.3 * machines; % would be .65 but assume some overspray/l
 bucket_cost = 760; % Dollars per bucket 
 coverage = 116.1288; %m2 this is 1250 sqft
 coating_cost = bucket_cost * surface_area_needed/coverage;
-possible_machines = coverage/1.3
+possible_machines = coverage/1.3;
 
 soln_cost = 30 * machines * cost_pin + coating_cost + 75; % Dollars (240 pins plus a bucket) for 8 machines
 e_cost = .06; % Dollars/kWhr
@@ -153,67 +146,49 @@ energy_save_day = daily_amount_saved_pm/3.600e6;
 daily_money_saved = machines * .06 * energy_save_day;
 days_to_even = soln_cost/daily_money_saved;
 %%
-full_day_old = 7 * cycles * Q_old_total
-new_cycles = 7 * cycles/ 6
-full_day_new = 6 * new_cycles * Q_new_total
-savings_new = (full_day_old - full_day_new)/full_day_old
+full_day_old = 7 * cycles * Q_old_total;
+new_cycles = 7 * cycles/ 6;
+full_day_new = 6 * new_cycles * Q_new_total;
+savings_new = (full_day_old - full_day_new)/full_day_old;
 
 
 %% Plotting
 figure
 %labels = {'Forced', 'Bottom' , 'Float', 'Top', 'Vertical', 'Radiation', 'Forced Pipe'}
-h1 = pie([Q_forced Q_bottom  Q_top  Q_float 2*Q_vertical Q_rad Q_forced_pipe])
+h1 = pie([Q_forced Q_bottom  Q_top  Q_float 2*Q_vertical Q_rad Q_forced_pipe]);
 colormap([230/256 230/256 .98; 52/256 190/256 235/256; ...
-          70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 105/256;  0/256 0/256 55/256; 1 1 1])
+          70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 115/256;  0/256 0/256 55/256; 1 1 1]);
 leg = legend('Forced: Spraying Air', ...
        'Bottom Plate: Top Surface' , 'Top Plate: Bottom Surface',  ...
        'Top Plate: Both Surfaces', 'Free Convection: Vertical Walls', ...
-       'Radiation', 'Forced: Air Through Platten')
+       'Radiation', 'Forced: Air Through Platten');
 
 leg.FontSize = 14;   
-set(h1(2:2:end),'FontSize',16)
-title('Single Cycle Original Heat Transfer Distribution')
+set(h1(2:2:end),'FontSize',16);
+title('Single Cycle Original Heat Transfer Distribution');
 ax = gca;
 ax.TitleFontSizeMultiplier = 1.6;
 
-
-%Q_L1 = daily_amount_saved_pm/cycles
-
-% figure
-% h2 = pie([Q_forced Q_bottom_n  Q_top_n Q_float 2*Q_vertical_n  Q_rad_n cycle_amt_saved],...
-%     [0 0 0 0 0 0 1])
-% colormap([230/256 230/256 .98; 52/256 190/256 235/256; ...
-%           70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 105/256;  0/256 0/256 55/256; .97 .97 1])
-% leg = legend('Forced: Spraying Air', 'Bottom Plate: Top Surface' , ...
-%        'Top Plate: Bottom Surface', 'Top Plate: Both Surfaces', ...
-%        'Free Convection: Vertical Walls', 'Radiation', 'Savings')
-% 
-% leg.FontSize = 14;
-% 
-% set(h2(2:2:end),'FontSize',16)
-% title('New Heat Transfer Distribution with Increased Productivity')
-% ax = gca;
-% ax.TitleFontSizeMultiplier = 1.8;
-
-
 figure
 pie([q_forced q_bottom q_float ...
-                 q_top  2*q_vertical  q_rad, q_forced_pipe])
+                 q_top  2*q_vertical  q_rad, q_forced_pipe]);
 legend('Forced: Spraying Air', 'Bottom Plate: Top Surface' , 'Top Plate: Both Surfaces', ...
         'Top Plate: Bottom Surface', 'Free Convection: Vertical Walls', 'Radiation', ...
-        'Forced: Air Through Platten')
-title('Original q Distribution')
+        'Forced: Air Through Platten');
+title('Original q Distribution');
+colormap([230/256 230/256 .98; 52/256 190/256 235/256; ...
+          70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 115/256;  0/256 0/256 55/256; .97 .97 1]);
 
 figure
 h2 = pie([Q_forced Q_bottom_n  Q_top_n Q_float 2*Q_vertical_n  Q_rad_n Q_loss],...
-    [0 0 0 0 0 0 1])
+    [0 0 0 0 0 0 1]);
 colormap([230/256 230/256 .98; 52/256 190/256 235/256; ...
-          70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 105/256;  0/256 0/256 55/256; .97 .97 1])
+          70/256 130/256 235/256; 0/256 50/256 190/256; 0 18/256 115/256;  0/256 0/256 55/256; .97 .97 1])
 legend('Forced: Spraying Air', 'Bottom Plate: Top Surface' , ...
        'Top Plate: Bottom Surface', 'Top Plate: Both Surfaces', ...
-       'Free Convection: Vertical Walls', 'Radiation', 'Savings')
-set(h2(2:2:end),'FontSize',16)
-title('Single Cycle New Heat Transfer Distribution')
+       'Free Convection: Vertical Walls', 'Radiation', 'Savings');
+set(h2(2:2:end),'FontSize',16);
+title('Single Cycle New Heat Transfer Distribution');
 ax = gca;
 ax.TitleFontSizeMultiplier = 1.6;
 
@@ -231,7 +206,7 @@ end
 % Forced convection from spraying air over surface
 function [q_forced, Q_total] = forced_convection(Ts, Ti, time, L_forced, area_forced)
     global kin_v Pr k 
-    velo2=20; %m/s
+    velo2=25; %m/s
     Rex = velo2 * L_forced/kin_v;
     nusselt_forced = .0296 * Rex ^.8 * Pr ^(1/3);
     h_forced = nusselt_forced * k./L_forced;
@@ -275,7 +250,7 @@ function [q_pipe_convection, Q_pipe_convection] = forced_pipe_convection(Ts, Ti,
     Re = rho * V * D/Dynamic_V;
 
     Nu = .023 * Re^.8 * Pr^.4;
-    h = Nu * k/ D % w/m^2 K
+    h = Nu * k/ D; % w/m^2 K
     area_affected = pi * D * length;
     q_pipe_convection = 30 *h .* area_affected * (Ts - Ti); % watts 
     Q_pipe_convection = q_pipe_convection * time;
