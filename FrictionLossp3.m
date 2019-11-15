ca2aba238b5f3b0ab2e93a8ee7cc580af59ee51d
Eric <edm54@case.edu>
2019-11-15 13:00:34 -0500

    friction?

--- FrictionLossp3.m
+++ FrictionLossp3.m
@@ -20,9 +20,8 @@ gravity = 9.81;
 diameter = 2.4; % inch
 diameter = 0.06096; % meter
 e_d_ratio = roughness/1000 * diameter;
-pressure = 10e6 %Pa
+
 temp = (15 + 129)/2 + 273;
-rho = refpropm('D','T',temp,'P', pressure /1e3, 'CO2');
 area = pi * (diameter/2)^2 ;
 md = 3.24:.1:12
 i = 1
@@ -75,15 +74,15 @@ i = 1
 % xlabel('Mass Flow Rate (kg/s)')
 % ylabel('Friction Head (m)')
 %% 
-leg =50:5:70%pascals, can set this one
-p6_val = 50e6:5e6:70e6
+leg =20:10:50%pascals, can set this one
+p3_val = 10e6:20e6:100e6
 i = 1
 k = 1e-12 ;% 
 A = 20 ;% m^-1
 L = 112 ;% m
 Pa = 32.5e6 ;
 Tb = 129 + 273; % Temp at bottom
-for p6  = 50e6:5e6:70e6
+for p3  = 20e6:10e6:50e6
     for j = 1:length(md)
 %         dynamic_v_top = refpropm('V','T',temp,'P',p6/1e3, 'CO2');
 %         rho(i,j) = refpropm('D','T',temp,'P',p6/1e3, 'CO2');
@@ -105,22 +104,24 @@ for p6  = 50e6:5e6:70e6
 %         head_loss_down(i,j) = (f_down(i,j) * L1 * velo_down^2) / (diameter * 2  * gravity);
 %         pressure_drop_down(i,j) = rho(i,j) * gravity * head_loss_down(i,j); %pa      
 %         
+        [p4(i,j), press_drop_down(i,j), gravity_gain_down(i,j)] = pressure_drop_down(md(j), p3);
+        %p4(i,j) = p3 - press_drop_down(i,j) + gravity_gain;
         
-        press_drop_up(i,j) = pressure_drop_up(md(j)-3.24, p6);
-        p5(i,j) = p6 + press_drop_up(i,j);
-        
-        density(i,j) = refpropm('D','T',Tb,'P',p5(i,j)/ 1e3, 'CO2');
-        dynamic_v(i,j) = refpropm('V','T',Tb,'P',p5(i,j)/1e3, 'CO2');
+        density(i,j) = refpropm('D','T',Tb,'P',p4(i,j)/ 1e3, 'CO2');
+        dynamic_v(i,j) = refpropm('V','T',Tb,'P',p4(i,j)/1e3, 'CO2');
       
         final_mdot(j) = md(j) - 3.24;
         Q(i,j) = final_mdot(j) / density(i,j);
         delta_p(i,j) = Q(i,j) * dynamic_v(i,j) * L/(k*A);
+        
         % + pa
-        p4(i,j) = p5(i,j) + delta_p(i,j);
-        gravity_gain = 0; % TODO
-        press_drop_down(i,j) = pressure_drop(md(j), p4(i,j));
-        p3(i,j) = p4(i,j) + press_drop_down(i,j) + gravity_gain;
-        delta_p(i,j) = p3(i,j) - p6;
+        p5(i,j) = p4(i,j) - delta_p(i,j);
+        [p6(i,j), press_drop_up(i,j), gravity_loss_up(i,j)] = pressure_drop_up(final_mdot(j), p5(i,j));
+        %press_drop_up(i,j) = pressure_drop_up(md(j)-3.24, p4(i,j));
+        
+        %p6 = p5(i,j) - press_drop_up(i,j) - gravity_gain
+        
+        final_delta_p(i,j) = p3 - p6(i,j) ;
         
     end
 %     for j = 1:length(md)
@@ -141,7 +142,7 @@ for p6  = 50e6:5e6:70e6
 end
 %%
 for iN = 1:length(leg)
-      legendCell{iN} = num2str(leg(iN),'P6=%-d');
+      legendCell{iN} = num2str(leg(iN),'P3=%-d');
 end
  
 %%
@@ -152,18 +153,39 @@ end
 % ylabel('Friction Head (m)')
 % legend(legendCell)
 
-figure
-hold on 
-plot(md, press_drop_up)
-title('Pressure loss due to friction in a pipe, up')
-xlabel('Mass Flow Rate Down(kg/s)')
-ylabel('Pressure Drop (Pa)')
-legend(legendCell)
+
 %legend('10' ,'20', '30', '40' ,'50')
 
+figure
+hold on 
+plot(final_mdot - 3.24, gravity_loss_up)
+title('Gravity loss up the pipe')
+xlabel('Mass Flow Rate Down(kg/s)')
+ylabel('Pressure loss (Pa)')
+%legend('10' ,'20', '30', '40' ,'50')
+legend(legendCell)
+%%
+figure
+hold on 
+plot(md, gravity_gain_down)
+title('Gravity gain down the pipe')
+xlabel('Mass Flow Rate Down(kg/s)')
+ylabel('Pressure gain (Pa)')
+%legend('10' ,'20', '30', '40' ,'50')
+legend(legendCell)
+%%
+figure
+hold on 
+plot(md, gravity_gain_down - gravity_loss_up)
+title('Gravity gain (difference)')
+xlabel('Mass Flow Rate Down(kg/s)')
+ylabel('Pressure gain (Pa)')
+%legend('10' ,'20', '30', '40' ,'50')
+legend(legendCell)
 
 
 
+%%
 figure
 hold on 
 plot(md, press_drop_down)
@@ -173,15 +195,6 @@ ylabel('Pressure Drop (Pa)')
 %legend('10' ,'20', '30', '40' ,'50')
 legend(legendCell)
 
-%%
-figure
-hold on
-plot(md, press_drop_down(2, :))
-legend('old', 'new')
-% plot(md, pressure_drop_down(2, :))
-% plot(md, pressure_drop_down1(2, :))
-%%
-%legend(legendCell)
 
 figure
 hold on 
@@ -191,6 +204,13 @@ xlabel('Mass Flow Rate Down(kg/s)')
 ylabel('Pressure Drop (Pa)')
 legend(legendCell)
 
+figure
+hold on 
+plot(md, press_drop_down)
+title('Pressure loss due to friction in a pipe, up')
+xlabel('Mass Flow Rate Down(kg/s)')
+ylabel('Pressure Drop (Pa)')
+legend(legendCell)
 % figure
 % hold on 
 % plot(md, pressure_drop_up)
@@ -209,30 +229,23 @@ legend(legendCell)
 % xlabel('Mass Flow Rate (kg/s)')
 % ylabel('Friction Head (m)')
 % legend(legendCell)
-%%
-figure
-plot(md , rho)
-title('Rho')
-xlabel('Mass Flow Rate (kg/s)')
-ylabel('rho kg')
-legend(legendCell)
 
 
 
 %%
 figure
-plot(md,p3) 
+plot(md,p6) 
 xlabel('Mass Flow Rate, Down')
-ylabel('Pressure 3 (pa)')
-title('P3 vs Mass Flow Rate for various P6')
+ylabel('Pressure 6 (pa)')
+title('P6 vs Mass Flow Rate for various P3')
 legend(legendCell)
 
 %%
 figure
-plot(md, delta_p) 
+plot(md, final_delta_p) 
 xlabel('Mass Flow Rate, Down')
 ylabel('Pressure Difference (3-6) (pa)')
-title('Delta P vs Mass Flow Rate for various P6')
+title('Final Delta P vs Mass Flow Rate for various P3')
 legend(legendCell)
 
 %%
@@ -240,41 +253,41 @@ figure
 plot(md, p4)
 xlabel('Mass Flow Rate, Down')
 ylabel('Pressure 4 (pa)')
-title('P4 vs Mass Flow Rate for various P6')
+title('P4 vs Mass Flow Rate for various P3')
 legend(legendCell)
 
 figure
 plot(md, delta_p)
 xlabel('Mass Flow Rate, Down')
-ylabel('Pelta P (pa)')
-title('Delta P vs Mass Flow Rate for various P6')
+ylabel('Delta P (pa)')
+title('Delta P Accross Sand vs Mass Flow Rate for various P3')
 legend(legendCell)
 
 figure
 plot(md, p5)
 xlabel('Mass Flow Rate, Down')
 ylabel('P5 (pa)')
-title('P5 vs Mass Flow Rate for various P6')
+title('P5 vs Mass Flow Rate for various P3')
 legend(legendCell)
-%%
-figure
-plot(md, density)
-xlabel('Mass Flow Rate, Down')
-ylabel('Density')
-title('Density vs Mass Flow Rate for various P6')
-legend(legendCell)
-%%
-figure
-plot(md, dynamic_v)
-xlabel('Mass Flow Rate, Down')
-ylabel('Dyn V')
-title('Dyn V vs Mass Flow Rate for various P6')
-legend(legendCell)
-
-figure
-plot(md, Q)
-xlabel('Mass Flow Rate, Down')
-ylabel('Vol Flow Rate')
-title('Vol Flow Rate vs Mass Flow Rate for various P6')
-legend(legendCell)
+% %%
+% figure
+% plot(md, density)
+% xlabel('Mass Flow Rate, Down')
+% ylabel('Density')
+% title('Density vs Mass Flow Rate for various P3')
+% legend(legendCell)
+% %%
+% figure
+% plot(md, dynamic_v)
+% xlabel('Mass Flow Rate, Down')
+% ylabel('Dyn V')
+% title('Dyn V vs Mass Flow Rate for various P3')
+% legend(legendCell)
+% 
+% figure
+% plot(md, Q)
+% xlabel('Mass Flow Rate, Down')
+% ylabel('Vol Flow Rate')
+% title('Vol Flow Rate vs Mass Flow Rate for various P3')
+% legend(legendCell)
 
---
+++ pressure_drop_up.m
@@ -0,0 +1,58 @@
+function [current_pressure, pressure_loss, gravity_gain] = pressure_drop_up(m_dot, pressure1)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+   
+    temp_range = [129+273 15+273];
+    len = [0 3200];
+    L1 = 3.2 * 1000; % meters
+    gravity = 9.81;
+    diameter = 0.06096; % meter
+    roughness = .05 ;% mm, from https://neutrium.net/fluid_flow/absolute-roughness/
+    % new = .02 -.05 mm, slightly corroded = .05 - .15, moderate corrosion =
+    % .15-1mm
+    e_d_ratio = roughness/1000 * diameter;
+    area = pi * (diameter/2)^2 ;
+    current_pressure = pressure1;
+    
+    i = 1;
+    R = 188.92 % Ideal gas Constant
+    %rho1 = refpropm('D','T',temp,'P',current_pressure/1e3, 'CO2');
+    pressure_loss = 0;
+    delta_l = 100;
+    gravity_gain = 0
+    % Start at top
+    % when each range covers h  --> h + delta_l
+    for height = 0 : delta_l : 3200 - delta_l
+        temp = interp1(len, temp_range, height + .5 *delta_l, 'linear');
+        dynamic_v1 = refpropm('V','T',temp,'P',current_pressure/1e3, 'CO2');
+        rho1(i) = refpropm('D','T',temp,'P',current_pressure/1e3, 'CO2');
+        
+        velo1 = m_dot/(rho1(i) * area);
+        velo(i) = m_dot/(rho1(i) * area);
+        reynolds = rho1(i) * velo1 * diameter/dynamic_v1;
+        f1 = -1.8 * log10((6.9 / reynolds) + (e_d_ratio/ 3.7)^1.); % turb only
+        f(i) = (1/f1)^2;
+
+        head_loss(i) = (f(i) * delta_l * velo1^2) / (diameter * 2  * gravity);
+        pressure_drop(i) = rho1(i) * gravity * head_loss(i); %pa
+        pressure_loss = pressure_loss + pressure_drop(i);
+        current_pressure = current_pressure - pressure_drop(i);
+        rho2 = refpropm('D','T',temp,'P',current_pressure/1e3, 'CO2');
+        velo2 = m_dot/(rho2 * area);
+        
+        velo_diff = (velo2^2 - velo1^2)/2;
+        % Gravity
+        %velo_diff = 0; % If we do small enough where rho does not change signifcantly(?)
+        % Negative delta l since 2 is below 1
+         p2 = current_pressure * exp((-1/(R * temp)) * (velo_diff +  gravity * delta_l));
+         gravity_gain = gravity_gain + current_pressure - p2 ;
+         current_pressure = p2; 
+         i = i + 1;
+    end
+%     disp(current_pressure)
+%     disp(gravity_gain)
+%     disp(pressure_loss)
+%     disp(velo)
+%      disp(pressure_loss)
+end
+
