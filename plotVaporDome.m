f33dd1ec611d1352e70c0abdd081711858a143e0
Eric <edm54@case.edu>
2019-11-25 17:33:24 -0500

    plots

--- plotVaporDome.m
+++ plotVaporDome.m
@@ -11,7 +11,7 @@ Temp = -20 + 273
 % from 0 c to 250c 
 
 
-p = 1e3 : .25e3 : 27.75e3; 
+p = 1e3 : .25e3 : 35e3; 
 
 %p = 4e3 : 1e3 : 60e3 
 t = 0 + 273:1:250 + 273;
@@ -32,28 +32,46 @@ for i = 1:length(p)
     for j = 1:length(h)
 %         Q(i,j) = refpropm('Q','T',t(j), 'P',p(i), 'CO2');
 %         X(i,j) = refpropm('X','T',t(j), 'P',p(i), 'CO2');
-        Q(i,j) = refpropm('Q','H',h(j), 'P',p(i), 'CO2');
-        X(i,j) = refpropm('X','H',h(j), 'P',p(i), 'CO2');
-        if Q(i,j) < .01 
-            liquid1 = [p(i)/1e3 liquid1 ];
-            liquid2 = [h(j) liquid2 ];
-        elseif Q(i,j) >= .01 && Q(i,j) <=1
-            mixed1 = [p(i)/1e3 mixed1];
-            mixed2 = [h(j) mixed2 ];
-        elseif Q(i,j) == 998 || Q(i,j) == -998 || (Q(i,j) > 1 && Q(i,j)< 5)
-            gas1 = [p(i)/1e3 gas1 ];
-            gas2 = [h(j) gas2 ];
+        
+        if p(i)<=27.75e3
+            Q(i,j) = refpropm('Q','H',h(j), 'P',p(i), 'CO2');
+            X(i,j) = refpropm('X','H',h(j), 'P',p(i), 'CO2');
+            if Q(i,j) < .01 
+                liquid1 = [p(i)/1e3 liquid1 ];
+                liquid2 = [h(j) liquid2 ];
+            elseif Q(i,j) >= .01 && Q(i,j) <=1
+                mixed1 = [p(i)/1e3 mixed1];
+                mixed2 = [h(j) mixed2 ];
+            elseif Q(i,j) == 998 || Q(i,j) == -998 || (Q(i,j) > 1 && Q(i,j)< 5)
+                gas1 = [p(i)/1e3 gas1 ];
+                gas2 = [h(j) gas2 ];
+            else
+                sup1 = [p(i)/1e3 sup1 ];
+                sup2 = [h(j) sup2 ];
+            end
         else
-            sup1 = [p(i)/1e3 sup1 ];
-            sup2 = [h(j) sup2 ];
+            %hcrit = 2.5e5
+            %liquid below
+            
+           if h(j)<2.53e5
+               liquid1 = [p(i)/1e3 liquid1 ];
+               liquid2 = [h(j) liquid2 ];
+           else
+               sup1 = [p(i)/1e3 sup1 ];
+               sup2 = [h(j) sup2 ];
+           end
+           
         end
+        
+        
+        
     end
 end
 %%
 x = figure;
-scatter(liquid2, liquid1  ,[], 'red', 'DisplayName', 'Liquid')
+scatter(liquid2, liquid1  ,[], 'blue', 'DisplayName', 'Liquid')
 hold on
-scatter(gas2, gas1, [], 'blue', 'DisplayName', 'Gas')
+scatter(gas2, gas1, [], 'red', 'DisplayName', 'Gas')
 hold on
 scatter(sup2, sup1, [], [.9 .5 0], 'DisplayName', 'Super Critical')
 hold on
