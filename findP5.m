c9400de223b1c91149c36756025661ff00ca1f91
Eric <edm54@case.edu>
2019-11-29 00:34:13 -0500

    if statment

--- findP5.m
+++ findP5.m
@@ -6,20 +6,29 @@ L = 112 ;% m
 Pa = 32.5e6 ;
 Tb = 129 + 273; % Temp at bottom
 
-[p3, friction_loss_down, gravity_gain_down, temp_down] = pressure_drop_down(md, p2, t2); 
+[p3, friction_loss_down, gravity_gain_down, temp_down] = pressure_drop_down(md, p2, t2);
 
-density = refpropm('D','T',Tb,'P',p3/ 1e3, 'CO2');
-dynamic_v = refpropm('V','T',Tb,'P',p3/1e3, 'CO2');
+if p3 < 32.5
+    p3 = NaN
+    p4 = NaN
+    p5= NaN
+    t5 = NaN
+    t4 = NaN
+    t3 = NaN
+else
+    density = refpropm('D','T',Tb,'P',p3/ 1e3, 'CO2');
+    dynamic_v = refpropm('V','T',Tb,'P',p3/1e3, 'CO2');
 
-t3 = temp_down(end);
+    t3 = temp_down(end);
 
-final_mdot = md - 3.24;
-Q = final_mdot/ density;
-delta_p = Q * dynamic_v* L/(k*A);
-t4 = 129+273;
-p4 = p3 - delta_p;
-[p5, friction_loss_up, gravity_loss_up, temp_top] = pressure_drop_up(final_mdot, p4);   
-t5 = temp_top;
-        
+    final_mdot = md - 3.24;
+    Q = final_mdot/ density;
+    delta_p = Q * dynamic_v* L/(k*A);
+    t4 = 129+273;
+    p4 = p3 - delta_p;
+    [p5, friction_loss_up, gravity_loss_up, temp_top] = pressure_drop_up(final_mdot, p4);   
+    t5 = temp_top;
+end
+
 end
 
--- findPumpPressure.m
+++ findPumpPressure.m
@@ -7,9 +7,9 @@ p2 = 15e6;
 
 % Mass flow rate 
 md = 3.24;
-md = 10
+%md = 10
 while (32.5e6 - p3 > 1e3)
-    [p3, press_drop_down, gravity_gain_down, temp_down] = pressure_drop_down(md, p2);
+    [p3, press_drop_down, gravity_gain_down, temp_down] = pressure_drop_down(md, p2, 30+273);
     p2 = p2 + .5 * (32.5e6 - p3);
 end
 
